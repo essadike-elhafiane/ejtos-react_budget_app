0924548e0885f5751ecbcb3acd88466317898007
Essadike  Elhafiane <eelhafia@e1r7p6.1337.ma>
2024-06-09 16:17:01 +0100

    add: decreaseAllocation

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -3,7 +3,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining , currency } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -59,7 +59,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                         <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-                    <label htmlFor='cost' style={{ marginLeft: '2rem'  , size: 10}}>£</label>
+                    <label htmlFor='cost' style={{ marginLeft: '2rem'  , size: 10}}>{currency}</label>
                     <input
                         required='required'
                         type='number'
